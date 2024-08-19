30676a6aa80918790fb8d5ad8ac901245fd9f385
TOMMY-z <bergentalthomaz@gmail.com>
2024-08-19 17:07:44 -0300

    Adicionado HelloReact

---
+++ helloReact/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className='titulo1'>HELLO React JS fofis</h1>   
+    <p className='paragrafo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Quae, architecto! Illo id temporibus incidunt, modi quia 
+      cum ipsa voluptates est sed reprehenderit quaerat autem, 
+      itaque voluptatem aspernatur deserunt repellat mollitia?</p>   
+    </>
+  )
+}
+
+export default App
