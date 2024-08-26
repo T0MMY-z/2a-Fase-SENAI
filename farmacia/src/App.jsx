ce188189f0ac50b564af475d5fa21a1ce9df63a0
TOMMY-z <bergentalthomaz@gmail.com>
2024-08-26 17:05:21 -0300

    farmacia

---
+++ farmacia/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='teste'>Farmárcia</h1>
+      <h2 className='descricao'>L!</h2>
+    </>
+  )
+}
+
+export default App
