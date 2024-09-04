166bf6403ad20b0848762ae42535cab64154f3f2
TOMMY-z <bergentalthomaz@gmail.com>
2024-09-04 16:22:53 -0300

    média notas

--- props_pagina/src/pages/Medianotas.jsx
+++ props_pagina/src/pages/Medianotas.jsx
@@ -1,20 +1,45 @@
-import React from 'react'
+import './Medianotas.css'
+import { useState } from 'react'
 import Titulo from '../components/Titulo'
 
 function Medianotas() {
+  const [nota1, setNota1] = useState('')
+  const [nota2, setNota2] = useState('')
+  const [nota3, setNota3] = useState('')
+  const [nota4, setNota4] = useState('')
+  const [medianotas, setMediaNotas] = useState(null)
 
-    let nota1 = Number(prompt('digite a primeira nota'))
-    let nota2 = Number(prompt('digite a segunda nota'))
-    let nota3 = Number(prompt('digite a terceira nota'))
-    let nota4 = Number(prompt('digite a quarta nota'))
+  function calcularMédia() {
+    const n1 = Number(nota1)
+    const n2 = Number(nota2)
+    const n3 = Number(nota3)
+    const n4 = Number(nota4)
 
-    let mediadasnotas = (nota1 + nota2 + nota3 + nota4) / 4
+    if (isNaN(n1) || isNaN(n2) || isNaN(n3) || isNaN(n4)) {
+      alert("As notas devem ser números válidos")
+      return
+    }
 
-    alert(mediadasnotas)
+    const mediaCalculada = (n1 + n2 + n3 + n4) / 4
+    setMediaNotas(mediaCalculada.toFixed(2))
+  }
 
   return (
-    <div>
-      <Titulo texto={"Vamos calcular a média das notas"} emoji={"✏"} />
+    <div className='notas'>
+      
+    <Titulo texto={"Sorteio de um número qualquer aí"} emoji={"🎲📈"}/>
+    <label> Nota 1 <input type="text" value={nota1} onChange={(e) => setNota1(e.target.value)}/></label>
+    <br />
+    <label> Nota 2 <input type="text" value={nota2} onChange={(e) => setNota2(e.target.value)}/></label> 
+    <br />
+    <label> Nota 3 <input type="text" value={nota3} onChange={(e) => setNota3(e.target.value)} /></label> 
+    <br />
+    <label> Nota 4 <input type="text" value={nota4} onChange={(e) => setNota4(e.target.value)} /></label> 
+    <br />
+    <br />
+    <button className='calcular-media' onClick={calcularMédia}>Calcular Média</button>
+    {medianotas !== null && <div>Média: {medianotas}</div>}
+
     </div>
   )
 }
